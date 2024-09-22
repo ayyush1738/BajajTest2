4605ac5f67b1a48b5d890f864cef3d4a715be2e1
ayyush1738 <singhrathoreayush824@gmail.com>
2024-09-22 21:36:39 +0530

    Frontend

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,12 +5,9 @@ const NavBar = () => {
         <nav className="bg-gray-800 p-4">
             <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center space-y-2 sm:space-y-0">
                 <div className="flex flex-col sm:flex-row items-center sm:space-x-4">
-                    <span className="text-white font-bold text-lg">AP21110010239</span>
+                    <span className="text-white font-bold text-lg">Bajaj Finserv Health</span>
                     <span className="text-gray-300">S Gyanesh Rao</span>
                 </div>
-                <h1 className="text-white font-bold text-xl text-center sm:text-left mt-2 sm:mt-0">
-                    Bajaj Finserv Health | Fullstack Qualifier | Assignment | 22nd September'24
-                </h1>
             </div>
         </nav>
     );
